efd9e45971ec105d8f02abf3a3cc9281aaac6a9c
Thomas Bush <thomasjjbush@gmail.com>
2025-03-13 20:03:40 +0000

    add link variant of clamped text component

--- src/components/clamped-text/clamped-text.tsx
+++ src/components/clamped-text/clamped-text.tsx
@@ -7,24 +7,39 @@ import style from './clamped-text.module.scss';
 import { Translation } from '@components/translation/translation';
 
 interface Props {
+  asLink?: boolean;
   children: string;
   className?: string;
   onClick?: () => void;
 }
 
-export function ClampedText({ className, children, onClick }: Props) {
+export function ClampedText({ asLink, className, children, onClick }: Props) {
   const [clamped, setClamped] = useState(true);
 
   return (
     <div className={className}>
-      <p className={cx(style.text, { [style.textClamped]: clamped })}>{children}</p>
-      <Button
-        label={<Translation id={clamped ? 'project.responsibilities.readMore' : 'project.responsibilities.readLess'} />}
-        onClick={() => {
-          onClick?.();
-          setClamped(!clamped);
-        }}
-      />
+      <p className={cx(style.text, { [style.textClamped]: clamped, [style.textAsLink]: asLink })}>{children}</p>
+      {asLink ? (
+        <span
+          className={style.textLink}
+          onClick={() => {
+            onClick?.();
+            setClamped(!clamped);
+          }}
+        >
+          <Translation id={clamped ? 'project.responsibilities.readMore' : 'project.responsibilities.readLess'} />
+        </span>
+      ) : (
+        <Button
+          label={
+            <Translation id={clamped ? 'project.responsibilities.readMore' : 'project.responsibilities.readLess'} />
+          }
+          onClick={() => {
+            onClick?.();
+            setClamped(!clamped);
+          }}
+        />
+      )}
     </div>
   );
 }
